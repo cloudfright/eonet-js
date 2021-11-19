62074921e2d43bc52ad283f09d5976f5b6007a2a
Ben Dyer <ben.dyer@hey.com>
2021-11-19 12:58:31 +0000

    Add some accessibility improvements

--- eonet-api.js
+++ eonet-api.js
@@ -2,28 +2,31 @@
 
 
 async function fetchCategoriesAsync() {
-    let response = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v3/categories')
+    let response = await fetch('https://eonet.gsfc.nasa.gov/api/v3/categories')
     let data = await response.json()
 
-    console.log('waiting');
     let categories = document.querySelector('#categories')
-    console.log(categories);
     categories.replaceChildren()
 
     for (const cateogry of data.categories) {
         let row = document.createElement('div');
         row.className = "row p-3 bg-light border"
-        
+        row.setAttribute('role', 'row')
+
         let title = document.createElement('div');
         title.className = "col-sm-4 fw-bold"
         title.textContent = cateogry.title
+        title.tabIndex = 0
+        title.setAttribute('role', 'cell')
         row.appendChild(title)
-        
+
         let desc = document.createElement('div');
         desc.className = "col-sm-8 text-muted"
         desc.textContent = cateogry.description
+        desc.tabIndex = 0
+        desc.setAttribute('role', 'cell')
         row.appendChild(desc)
-        
+
         categories.appendChild(row)
     }
 }
