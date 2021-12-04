09e5b148235e16a92a7d6b23de103e885ddbcf09
Ben Dyer <ben.dyer@hey.com>
2021-12-04 20:09:53 +0000

    Refactor dashboard logic

--- eonet-api.js
+++ eonet-api.js
@@ -1,190 +1,108 @@
 
- // This list the id's of the EONET categories 
- const categories = {
-  DROUGHT: 'drought',
-  DUSTHAZE: 'dustHaze',
-  EARTHQUAKES: 'earthquakes',
-  FLOODS: 'floods',
-  LANDSLIDES: 'landslides',
-  MANMADE: 'manmade',
-  SEALAKEICE: 'seaLakeIce',
-  SERVERSTORMS: 'severeStorms',
-  SNOW: 'snow',
-  TEMPEXTREMES: 'tempExtremes',
-  VOLCANOES: 'volcanoes',
-  WATERCOLOUR: 'waterColor',
-  WILDFIRES: 'wildfires'
-}
-
-// the display name of the EONET categories
-const categoryDisplayName = {
-  DROUGHT: 'Drought',
-  DUSTHAZE: 'Dust Haze',
-  EARTHQUAKES: 'Earthquakes',
-  FLOODS: 'Floods',
-  LANDSLIDES: 'Landslides',
-  MANMADE: 'Manmade',
-  SEALAKEICE: 'Sea Lake Ice',
-  SERVERSTORMS: 'Severe Storms',
-  SNOW: 'Snow',
-  TEMPEXTREMES: 'Temp Extremes',
-  VOLCANOES: 'Volcanoes',
-  WATERCOLOUR: 'Water Colour',
-  WILDFIRES: 'Wildfires'
-}
-
 // fetch and render data for home page 
 async function fetchCategoriesAsync() {
-    let response = await fetch('https://eonet.gsfc.nasa.gov/api/v3/categories')
-    let data = await response.json()
+  let response = await fetch('https://eonet.gsfc.nasa.gov/api/v3/categories')
+  let data = await response.json()
 
-    let categories = document.querySelector('#categories')
-    categories.replaceChildren()
+  let categories = document.querySelector('#categories')
+  categories.replaceChildren()
 
-    for (const cateogry of data.categories) {
-        let row = document.createElement('div');
-        row.className = "row p-3 bg-light border"
-        row.setAttribute('role', 'row')
+  for (const cateogry of data.categories) {
+    let row = document.createElement('div');
+    row.className = "row p-3 bg-light border"
+    row.setAttribute('role', 'row')
 
-        let title = document.createElement('div');
-        title.className = "col-sm-4 fw-bold"
-        title.textContent = cateogry.title
-        title.tabIndex = 0
-        title.setAttribute('role', 'cell')
-        row.appendChild(title)
+    let title = document.createElement('div');
+    title.className = "col-sm-4 fw-bold"
+    title.textContent = cateogry.title
+    title.tabIndex = 0
+    title.setAttribute('role', 'cell')
+    row.appendChild(title)
 
-        let desc = document.createElement('div');
-        desc.className = "col-sm-8 text-muted"
-        desc.textContent = cateogry.description
-        desc.tabIndex = 0
-        desc.setAttribute('role', 'cell')
-        row.appendChild(desc)
+    let desc = document.createElement('div');
+    desc.className = "col-sm-8 text-muted"
+    desc.textContent = cateogry.description
+    desc.tabIndex = 0
+    desc.setAttribute('role', 'cell')
+    row.appendChild(desc)
 
-        categories.appendChild(row)
-    }
+    categories.appendChild(row)
+  }
 }
 
 
-// fetch and render data for the dashboard page
 async function fetchDashboardDataAsync() {
-    let response = await fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open')
-    let data = await response.json()
-  
-    let eventMap = new Map ([
-      [categoryDisplayName.DROUGHT, 0],
-      [categoryDisplayName.DUSTHAZE, 0],
-      [categoryDisplayName.EARTHQUAKES, 0],
-      [categoryDisplayName.FLOODS, 0],
-      [categoryDisplayName.LANDSLIDES, 0],
-      [categoryDisplayName.MANMADE, 0],
-      [categoryDisplayName.SEALAKEICE, 0],
-      [categoryDisplayName.SERVERSTORMS, 0],
-      [categoryDisplayName.SNOW, 0],
-      [categoryDisplayName.TEMPEXTREMES, 0],
-      [categoryDisplayName.VOLCANOES, 0],
-      [categoryDisplayName.WATERCOLOUR, 0],
-      [categoryDisplayName.WILDFIRES, 0]
-    ])
+  let response = await fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open')
+  let data = await response.json()
 
-    // iterate through the event data and keep running totals of events per category
-    for (const event of data.events) {
-      switch (event.categories[0].id) // count up the event id
-      {
-        case categories.DROUGHT:
-          incrementEvent(eventMap, categoryDisplayName.DROUGHT);
-          break;
-        case categories.DUSTHAZE: 
-          incrementEvent(eventMap, categoryDisplayName.DUSTHAZE);
-          break;
-        case categories.EARTHQUAKES:
-         incrementEvent(eventMap, categoryDisplayName.EARTHQUAKES);
-          break;
-        case categories.FLOODS:
-          incrementEvent(eventMap, categoryDisplayName.FLOODS);
-          break;
-        case categories.LANDSLIDES:
-          incrementEvent(eventMap, categoryDisplayName.LANDSLIDES);
-          break;
-        case categories.MANMADE:
-          incrementEvent(eventMap, categoryDisplayName.MANMADE);
-          break;
-        case categories.SEALAKEICE:
-          incrementEvent(eventMap, categoryDisplayName.SEALAKEICE);
-          break;
-        case categories.SERVERSTORMS:
-          incrementEvent(eventMap, categoryDisplayName.SERVERSTORMS);
-          break;
-        case categories.SNOW:
-          incrementEvent(eventMap, categoryDisplayName.SNOW);
-          break;
-        case categories.TEMPEXTREMES:
-          incrementEvent(eventMap, categoryDisplayName.TEMPEXTREMES);
-          break;
-        case categories.VOLCANOES:
-          incrementEvent(eventMap, categoryDisplayName.VOLCANOES);
-          break;
-        case categories.WATERCOLOUR:
-          incrementEvent(eventMap, categoryDisplayName.WATERCOLOUR);
-          break;
-        case categories.WILDFIRES:
-          incrementEvent(eventMap, categoryDisplayName.WILDFIRES);
-          break;
-      }
-    }
-
-    // sort the map containing the event counts (number of events descending)
-    const unsortedArray = [...eventMap]
-    const sortedArray = unsortedArray.sort(([key1, value1], [key2, value2]) => {
-      if (value2 > value1) return 1
-      else if (value2 < value1) return -1
-      else return 0 
-    })
-    const sortedEvents = new Map(sortedArray)
-
-    // locate the ordered list element we will programmatically append list items to and clear any remove any exsiting list items 
-    let currentEvents = document.querySelector('#currentevents')
-    currentEvents.replaceChildren()
-
-    // iterate through the sorted event list and create HTML elements
-    for (let [name, count] of sortedEvents.entries()) {
-    
-      let listItem = document.createElement('li');
-      listItem.className = "list-group-item d-flex justify-content-between align-items-center"
-      listItem.setAttribute('role', 'row')
-
-      let title = document.createElement('div');
-      title.className = "fw-bold m-2"
-      title.textContent = name
-      title.tabIndex = 0
-      title.setAttribute('role', 'cell')
-      listItem.appendChild(title)
-
-      let eventTotal = document.createElement('span')
-      if (count > 0) // only display blue badges for event counts greater than 0
-        eventTotal.className = "badge bg-primary rounded-pill m-2"
-      else
-       eventTotal.className = "badge bg-secondary rounded-pill m-2"
-       
-      eventTotal.textContent = count
-      eventTotal.tabIndex = 0   
-      eventTotal.setAttribute('role', 'cell')
-      listItem.appendChild(eventTotal)
-
-      currentEvents.appendChild(listItem)
-    }
+  let categoryCount = {
+    'Drought': 0,
+    'Dust Haze': 0,
+    'Earthquakes': 0,
+    'Floods': 0,
+    'Landslides': 0,
+    'Manmade': 0,
+    'Sea Lake Ice': 0,
+    'Severe Storms': 0,
+    'Snow': 0,
+    'Temp Extremes': 0,
+    'Volcanoes': 0,
+    'Water Colour': 0,
+    'Wildfires': 0
   }
 
-  // helper function increment event count
-  function incrementEvent(map, eventId) {
-    map.set(eventId, map.get(eventId) + 1)
+  // count up event category titles 
+  data.events.reduce(function (categories, event) {
+
+    let categoryTitle = event.categories[0].title
+
+    if (categoryTitle in categories) {
+      categories[categoryTitle]++
+    }
+    return categories
+  }, categoryCount)
+
+  // convert to array
+  var currentEventTotals = []
+  for (category in categoryCount) {
+    currentEventTotals.push({ title: category, count: categoryCount[category] })
   }
 
+  // sort by count descending
+  currentEventTotals.sort((a, b) => {
+    return b.count - a.count;
+  });
 
+  // locate the ordered list element we will programmatically append list items to and clear any remove any exsiting list items 
+  let currentEvents = document.querySelector('#currentevents')
+  currentEvents.replaceChildren()
 
-  /*
-  https://eonet.gsfc.nasa.gov/api/v3/events?category=wildfires&days=30
+  // iterate through the sorted event list and create HTML elements
+  for (let category of currentEventTotals) {
 
-  https://www.chartjs.org/docs/latest/developers/updates.html
+    let listItem = document.createElement('li');
+    listItem.className = "list-group-item d-flex justify-content-between align-items-center"
+    listItem.setAttribute('role', 'row')
 
-  https://www.w3docs.com/snippets/javascript/how-to-format-a-javascript-date.html
-  */
+    let title = document.createElement('div');
+    title.className = "fw-bold m-2"
+    title.textContent = category.title
+    title.tabIndex = 0
+    title.setAttribute('role', 'cell')
+    listItem.appendChild(title)
+
+    let eventTotal = document.createElement('span')
+    if (category.count > 0) // only display blue badges for event counts greater than 0
+      eventTotal.className = "badge bg-primary rounded-pill m-2"
+    else
+      eventTotal.className = "badge bg-secondary rounded-pill m-2"
+
+    eventTotal.textContent = category.count
+    eventTotal.tabIndex = 0
+    eventTotal.setAttribute('role', 'cell')
+    listItem.appendChild(eventTotal)
+
+    currentEvents.appendChild(listItem)
+  }
+
+}
